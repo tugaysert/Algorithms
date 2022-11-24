2ef0f02cd402687e868a715e1c1e53e81561c7ec
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-11-24 15:56:58 +0300

    reverse link list

--- src/LeetCode/ReverseLinkedList.java
+++ src/LeetCode/ReverseLinkedList.java
@@ -1,4 +1,48 @@
 package LeetCode;
 
 public class ReverseLinkedList {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ListNode listNode = new ListNode(3);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(0);
+        ListNode listNode4 = new ListNode(-4);
+
+
+        listNode.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+
+
+        System.out.println(reverseNode(listNode));
+    }
+
+    public static ListNode reverseNode(ListNode head) {
+
+        // 1 2 3 4 5
+
+
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+
+        }
+        return head;
+
+
+    }
 }
